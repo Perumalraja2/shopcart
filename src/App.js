e6ab7ab3633f97ca3262f1a2003e3bd58fe943dd
Perumalraja2 <perumalraja2.m@gmail.com>
2023-06-06 20:17:08 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import NavBar from "./component/NavBar";
 import Header from "./component/Header";
 
 function App() {
+  let data = [{
+    product:"Fancy product",
+    price:"$40 - $80"
+  },
+  {
+    product:"Special Item",
+    price:"$25"
+  },
+  {
+    product:"Sale item",
+    price:"$18"
+  },
+  {
+    product:"Popular item",
+    price:"$45"
+  }
+]
   let[cartvalue,setCartvalue]=useState(0);
   return <>
 <NavBar cartvalue={cartvalue}/>
 <Header/>
-<section class="py-5">
+<section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-<Card setCartvalue={setCartvalue}/>
-<Card setCartvalue={setCartvalue}/>
-<Card setCartvalue={setCartvalue}/>
-<Card setCartvalue={setCartvalue}/>
-
+<Card setCartvalue={setCartvalue} pro={data[0].product}  pri ={data[0].price }/>
+<Card setCartvalue={setCartvalue} pro={data[1].product}  pri ={data[1].price }/>
+<Card setCartvalue={setCartvalue} pro={data[2].product}  pri ={data[2].price }/>
+<Card setCartvalue={setCartvalue} pro={data[3].product}  pri ={data[3].price }/>
 
 </div>
 </div>
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 
-function Card({setCartvalue}) {
+function Card({setCartvalue,pro,pri}) {
   
   let [toggle,setToggle]=useState(true);
   return <>
@@ -9,8 +9,8 @@ function Card({setCartvalue}) {
 <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
 <div className="card-body p-4">
 <div className="text-center">
-<h5 className="fw-bolder">Fancy Product</h5>
-$40.00 - $80.00
+<h5 className="fw-bolder">{pro}</h5>
+{pri}
 </div>
 </div>
 <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
